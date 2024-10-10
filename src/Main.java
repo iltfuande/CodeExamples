5e3aa247f986904082534f0932680c7b4c14eff3
iltfuande <iltfuande@gmail.com>
2024-10-10 16:03:40 +0300

    Everyday tasks

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
-import leetcode.design.BSTIterator;
-import leetcode.design.util.TreeNode;
+import leetcode.design.MedianFinder;
 
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(7);
-        root.left = new TreeNode(3);
-        root.right = new TreeNode(15);
-        root.right.left = new TreeNode(9);
-        root.right.right = new TreeNode(20);
-
-        BSTIterator iterator = new BSTIterator(root);
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        System.out.println(iterator.hasNext());
-        System.out.println(iterator.next());
-        System.out.println(iterator.hasNext());
-        System.out.println(iterator.next());
-        System.out.println(iterator.hasNext());
-        System.out.println(iterator.next());
-        System.out.println(iterator.hasNext());
+        var root = new MedianFinder();
+        root.addNum(2);
+        root.addNum(3);
+        root.addNum(-5);
+        root.addNum(8);
+        System.out.println(root.findMedian());
     }
 }
