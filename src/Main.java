da2949b300c831727087127529de8011e786c301
iltfuande <iltfuande@gmail.com>
2024-10-17 16:00:38 +0300

    Everyday tasks

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,14 @@
 import leetcode.design.Codec;
+import leetcode.design.NumArray;
 import leetcode.design.WordDictionary;
 import leetcode.design.util.TreeNode;
 
 public class Main {
 
     public static void main(String[] args) {
-        var root = new Codec();
-        var node = new TreeNode();
-
-        node.val = 1;
-        var node2 = new TreeNode();
-        node2.val = 2;
-        var node3 = new TreeNode();
-        node3.val = 3;
-        var node4 = new TreeNode();
-        node4.val = 4;
-        var node5 = new TreeNode();
-        node5.val = 5;
-        var node6 = new TreeNode();
-        node6.val = 99;
-
-        node.left = node2;
-        node.right = node3;
-        node3.left = node4;
-        node3.right = node5;
-        node5.right = node6;
-
-        System.out.println(root.serialize(node));
-        System.out.println(root.deserialize(""));
-        System.out.println(node);
+        var root = new NumArray(new int[] {-2, 0, 3, -5, 2, -1});
+        System.out.println(root.sumRange(0, 2));
+        System.out.println(root.sumRange(2, 5));
+        System.out.println(root.sumRange(0, 5));
     }
 }
